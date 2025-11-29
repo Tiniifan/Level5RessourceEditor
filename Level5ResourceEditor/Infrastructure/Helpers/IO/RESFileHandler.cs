4abe8463c3e19b63e65a3486760458f9bdbb9be6
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2025-11-29 13:22:07 +0100

    Add RESFileHandler and XRESFileHandler
    To manage save function

---
+++ Level5ResourceEditor/Infrastructure/Helpers/IO/RESFileHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StudioElevenLib.Level5.Resource;
+using StudioElevenLib.Level5.Resource.RES;
+using StudioElevenLib.Level5.Resource.XRES;
+using StudioElevenLib.Level5.Resource.Types;
+using StudioElevenLib.Level5.Resource.Types.Scene3D;
+
+namespace Level5ResourceEditor.Infrastructure.Helpers.IO
+{
+    public class RESFileHandler
+    {
+        public void SaveScene3D(Dictionary<RESType, List<RESElement>> items, string filePath, string magic)
+        {
+            // Filter only materials and nodes for Scene3D
+            var filteredItems = new Dictionary<RESType, List<RESElement>>();
+
+            // Add materials
+            foreach (var materialType in RESSupport.Materials)
+            {
+                if (items.ContainsKey(materialType))
+                {
+                    // Special handling for TextureData - only keep RESTextureData
+                    if (materialType == RESType.TextureData)
+                    {
+                        var textureData = items[materialType]
+                            .Where(e => e.GetType() == typeof(RESTextureData))
+                            .ToList();
+
+                        if (textureData.Count > 0)
+                        {
+                            filteredItems[materialType] = textureData;
+                        }
+                    }
+                    else
+                    {
+                        filteredItems[materialType] = new List<RESElement>(items[materialType]);
+                    }
+                }
+            }
+
+            // Add nodes
+            foreach (var nodeType in RESSupport.Nodes)
+            {
+                if (items.ContainsKey(nodeType))
+                {
+                    filteredItems[nodeType] = new List<RESElement>(items[nodeType]);
+                }
+            }
+
+            // Create RES and save
+            var res = new RES { Items = filteredItems };
+            res.Save(magic, filePath);
+        }
+
+        public void SaveScene2D(Dictionary<RESType, List<RESElement>> items, string filePath, string magic)
+        {
+            throw new NotImplementedException("Scene2D save is not yet implemented");
+        }
+    }
+}
---
+++ Level5ResourceEditor/Infrastructure/Helpers/IO/XRESFileHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StudioElevenLib.Level5.Resource;
+using StudioElevenLib.Level5.Resource.XRES;
+using StudioElevenLib.Level5.Resource.Types;
+using StudioElevenLib.Level5.Resource.Types.Scene3D;
+
+namespace Level5ResourceEditor.Infrastructure.Helpers.IO
+{
+    public class XRESFileHandler
+    {
+        public void SaveScene3D(Dictionary<RESType, List<RESElement>> items, string filePath, string magic)
+        {
+            // Filter and prepare items for XRES Scene3D
+            var filteredItems = new Dictionary<RESType, List<RESElement>>();
+
+            // Process materials from XRESSupport.TypeOrder
+            foreach (var materialType in XRESSupport.TypeOrder)
+            {
+                // Only process materials for Scene3D
+                if (IsScene3DMaterial(materialType))
+                {
+                    if (items.ContainsKey(materialType))
+                    {
+                        // Special handling for TextureData - only keep XRESTextureData
+                        if (materialType == RESType.TextureData)
+                        {
+                            var textureData = items[materialType]
+                                .Where(e => e.GetType() == typeof(XRESTextureData))
+                                .ToList();
+
+                            filteredItems[materialType] = textureData;
+                        }
+                        else
+                        {
+                            filteredItems[materialType] = new List<RESElement>(items[materialType]);
+                        }
+                    }
+                    else
+                    {
+                        // Add empty list if key doesn't exist
+                        filteredItems[materialType] = new List<RESElement>();
+                    }
+                }
+            }
+
+            // Process nodes for Scene3D
+            var nodeTypes = new[] { RESType.MeshName, RESType.Bone, RESType.AnimationMTN2,
+                RESType.AnimationIMN2, RESType.AnimationMTM2, RESType.Shading,
+                RESType.Properties, RESType.MTNINF, RESType.IMMINF, RESType.MTMINF, RESType.Textproj };
+
+            foreach (var nodeType in nodeTypes)
+            {
+                if (items.ContainsKey(nodeType))
+                {
+                    filteredItems[nodeType] = new List<RESElement>(items[nodeType]);
+                }
+                else
+                {
+                    // Add empty list if key doesn't exist
+                    filteredItems[nodeType] = new List<RESElement>();
+                }
+            }
+
+            // Create XRES and save
+            var xres = new XRES { Items = filteredItems };
+            xres.Save(magic, filePath);
+        }
+
+        public void SaveScene2D(Dictionary<RESType, List<RESElement>> items, string filePath, string magic)
+        {
+            throw new NotImplementedException("Scene2D save is not yet implemented");
+        }
+
+        private bool IsScene3DMaterial(RESType type)
+        {
+            return type == RESType.MaterialTypeUnk1 ||
+                   type == RESType.Material1 ||
+                   type == RESType.Material2 ||
+                   type == RESType.TextureData ||
+                   type == RESType.MaterialTypeUnk2 ||
+                   type == RESType.MaterialData;
+        }
+    }
+}
